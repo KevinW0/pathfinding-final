b54699bd21e13125aebc67aef91b05dd4bd075ca
Kevin <kevin.wang.02@outlook.com>
2024-06-25 20:19:54 -0400

    feat: :sparkles: Add help dialog.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Grid from './Grid';
 import Header from './Header';
 import Footer from './Footer';
 import { useState } from 'react';
+import { HelpDialog } from './HelpDialog';
 
 function App() {
     const [speed, setSpeed] = useState(0);
@@ -12,6 +13,7 @@ function App() {
     return (
         <>
             <Header controlsDisabled={controlDisabled} setSpeed={setSpeed} algorithms={algorithms} resetAvailableOnly={resetAvailableOnly}></Header>
+            <HelpDialog />
             <Grid
                 speed={speed}
                 disableControls={setControlDisabled}
---
+++ src/HelpDialog.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Dialog, DialogTitle } from '@mui/material';
+import { PiMouseLeftClickFill, PiMouseRightClickFill } from 'react-icons/pi';
+import { HiCursorClick } from 'react-icons/hi';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+export const HelpDialog = () => {
+    const [helpDialog, setHelpDialog] = useState(true);
+    return (
+        <Dialog open={helpDialog} onClose={() => setHelpDialog(false)}>
+            <DialogTitle style={{ textAlign: 'center' }}>Tutorial</DialogTitle>
+            <div style={{ margin: '0px 10px 10px 10px' }}>
+                <p><PiMouseLeftClickFill style={{ display: 'inline' }} />Left click to mark a starting node.</p>
+                <p><PiMouseRightClickFill style={{ display: 'inline' }} />Right click to mark a starting node.</p>
+                <p><HiCursorClick style={{ display: 'inline' }} />Click the green button to begin the visualization!</p>
+                <p><AiOutlineLoading3Quarters style={{ display: 'inline' }} />When the grid loads, click outside the dialog to enter the main application!</p>
+            </div>
+        </Dialog>
+    )
+}
