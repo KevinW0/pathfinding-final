96b1387a5e7be1516dcb8bbebd0a8a2017da55fd
Kevin <kevin.wang.02@outlook.com>
2024-06-25 20:25:19 -0400

    fix: :bug: Fix typo, spacing.

--- src/HelpDialog.jsx
+++ src/HelpDialog.jsx
@@ -11,9 +11,9 @@ export const HelpDialog = () => {
             <DialogTitle style={{ textAlign: 'center' }}>Tutorial</DialogTitle>
             <div style={{ margin: '0px 10px 10px 10px' }}>
                 <p><PiMouseLeftClickFill style={{ display: 'inline' }} />Left click to mark a starting node.</p>
-                <p><PiMouseRightClickFill style={{ display: 'inline' }} />Right click to mark a starting node.</p>
+                <p><PiMouseRightClickFill style={{ display: 'inline' }} />Right click to mark a target node.</p>
                 <p><HiCursorClick style={{ display: 'inline' }} />Click the green button to begin the visualization!</p>
-                <p><AiOutlineLoading3Quarters style={{ display: 'inline' }} />When the grid loads, click outside the dialog to enter the main application!</p>
+                <p><AiOutlineLoading3Quarters style={{ display: 'inline' }} /> When the grid loads, click outside the dialog to enter the main application!</p>
             </div>
         </Dialog>
     )
